d2607b9abe091e04f3b8ea9cacaacdb853b1647e
arturfsilvazin12-tech <arturfsilvazin12@gmail.com>
2026-09-01 20:00:15 -0300

    feat: registrar core client V0.5

---
+++ core/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name 'silva-vision-core'
+author 'Silva Vision'
+description 'Silva Vision V0.5 client visual runtime layer'
+version '0.5.0-dev'
+
+client_scripts {
+    'client/visual_runtime.lua'
+}
